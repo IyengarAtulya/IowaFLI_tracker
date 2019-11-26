62ec2a4c54ad27a0b910544ff2f50a1e2241985f
Atulya Iyengar <48802041+IyengarAtulya@users.noreply.github.com>
2019-11-26 11:54:36 -0600

    Delete getIowaFliDefaults.m

--- getIowaFliDefaults.m
+++
@@ -1,7 +0,0 @@
-function abc=getIowaFliDefaults(FILENAME)
-def=importdata(FILENAME);
-for i=4:numel(def)
-tok(i)=regexp(def{i},':\s*[\d*.]*\s*%','match');
-n=regexp(tok{i},'[\d*.]*','match');
-abc(i)=str2num(n{1});
-end
